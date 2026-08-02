a1e62181ce61010e7fcbbe8ce8cc47d15e1379af
korovaichikovmark228-a11y <korovaichikovmark228@gmail.com>
2026-08-02 11:26:03 +0300

    add app/layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
+import Script from "next/script";
 import { Onest } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+
+const YM_ID = 111240069;
 
 const onest = Onest({
   subsets: ["latin", "cyrillic"],
@@ -32,6 +35,21 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<e.scripts.length;j++){if(e.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=${YM_ID}','ym');ym(${YM_ID},'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",accurateTrackBounce:true,trackLinks:true});`}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src={`https://mc.yandex.ru/watch/${YM_ID}`}
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
       </body>
     </html>
   );
